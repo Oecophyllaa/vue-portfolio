8c2162ef563ac2b3d818830260c20213277afccc
Oecophyllaa <rangga.godzila@gmail.com>
2025-08-02 09:45:54 +0700

    feat: Integrate @vercel/speed-insights for performance tracking

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
 import { RouterView } from 'vue-router'
 import { Analytics } from '@vercel/analytics/vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 
 export default {
   name: 'App',
   components: {
     RouterView,
     Analytics,
+    SpeedInsights,
   },
 }
 </script>
 
 <template>
   <Analytics />
+  <SpeedInsights />
 
   <div id="app">
     <RouterView />
